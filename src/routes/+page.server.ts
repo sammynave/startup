cad9a22cb7360e9159395d0f4cce4f4bf1303d3d
Sammy Nave <sammy.nave@gmail.com>
2023-10-25 08:43:54 -0400

    redirect from home

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,5 +5,7 @@ export const load: PageServerLoad = async ({ locals }) => {
 	const session = await locals.auth.validate();
 	if (session?.user?.userId) {
 		throw redirect(302, '/app');
+	} else {
+		throw redirect(302, '/sign-up');
 	}
 };
