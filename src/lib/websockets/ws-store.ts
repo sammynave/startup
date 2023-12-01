b228ed16ccb79c1c241fdce7407c86bad3f9beb6
Sammy Nave <sammy.nave@gmail.com>
2023-11-30 22:51:57 -0500

    try this

--- src/lib/websockets/ws-store.ts
+++ src/lib/websockets/ws-store.ts
@@ -1,38 +1,33 @@
 import { browser } from '$app/environment';
 
 type Subscription = (value: WebSocket | null) => void;
+
+function open(url: string, subscriptions: Set<Subscription>) {
+	const ws = new WebSocket(url);
+	ws.addEventListener('error', (error) => console.error(error));
+	ws.addEventListener('open', () => subscriptions.forEach((subscription) => subscription(ws)));
+	return ws;
+}
+
 export function wsStore({ url }: { url: string }) {
 	const subscriptions = new Set<Subscription>();
 
 	let ws: WebSocket | null = null;
-
-	function open() {
-		if (ws) {
-			return;
-		}
-		ws = new WebSocket(url);
-		ws.addEventListener('error', (error) => console.error(error));
-		ws.addEventListener('open', () => subscriptions.forEach((subscription) => subscription(ws)));
-	}
-
-	function close() {
-		if (ws) {
-			ws.close();
-			ws = null;
-		}
-	}
 
 	return {
 		subscribe(subscription: Subscription) {
 			if (!browser) {
 				return () => undefined;
 			}
-			open();
+
+			ws = ws === null ? open(url, subscriptions) : ws;
 			subscriptions.add(subscription);
+
 			return () => {
 				subscriptions.delete(subscription);
-				if (subscriptions.size === 0) {
-					close();
+				if (subscriptions.size === 0 && ws !== null) {
+					ws.close();
+					ws = null;
 				}
 			};
 		},
