035a5b6733f67a17c237efa49875dd0b6f5f80b6
Sammy Nave <sammy.nave@gmail.com>
2023-12-01 09:38:43 -0500

    extract method

--- src/lib/websockets/chat-store.ts
+++ src/lib/websockets/chat-store.ts
@@ -8,32 +8,30 @@ export type Message = {
 	username?: string;
 };
 
+const attachListener = (initialValue: Message[]) => ($ws, set) => {
+	let listenerAdded = false;
+	if ($ws && !listenerAdded) {
+		let messages: Message[] = initialValue;
+		const addMessage = (message: Message) => {
+			messages = [...messages, message];
+		};
+		$ws.addEventListener('message', (event) => {
+			listenerAdded = true;
+			if (typeof event.data === 'string') {
+				const data = JSON.parse(event.data);
+				if (['message', 'connect', 'disconnect'].includes(data.type)) {
+					addMessage(data);
+					set(messages);
+				}
+			}
+		});
+	}
+};
+
 // NOTE!!! In local dev, when you save this file, HMR removes the last few messages when reloading.
 // that's because initialValue isn't updated from the server. Not a problem in prod
 export function chatStore(ws: ReturnType<typeof wsStore>, initialValue: Message[]) {
-	const messages = derived(
-		ws,
-		($ws, set) => {
-			let listenerAdded = false;
-			if ($ws && !listenerAdded) {
-				let messages: Message[] = initialValue;
-				const addMessage = (message: Message) => {
-					messages = [...messages, message];
-				};
-				$ws.addEventListener('message', (event) => {
-					listenerAdded = true;
-					if (typeof event.data === 'string') {
-						const data = JSON.parse(event.data);
-						if (['message', 'connect', 'disconnect'].includes(data.type)) {
-							addMessage(data);
-							set(messages);
-						}
-					}
-				});
-			}
-		},
-		initialValue
-	);
+	const messages = derived(ws, attachListener(initialValue), initialValue);
 
 	return {
 		subscribe: messages.subscribe,
